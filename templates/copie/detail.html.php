77d7ec911451b15931a440a40e1c3b0308eb4286
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2026-09-02 19:08:56 +0000

    feat: ajouter les vues (formulaire, liste, détail et erreurs de validation)

---
+++ templates/copie/detail.html.php
@@ -0,0 +1,13 @@
+<div class="container mt-4">
+    <h2>Détail de la copie</h2>
+
+    <div class="card mt-3">
+        <div class="card-body">
+            <p><strong>Note Brute :</strong> 14.0</p>
+            <p><strong>Note Finale :</strong> 12.0</p>
+            <p><strong>Pénalité Appliquée :</strong> 2.0</p>
+            <p><strong>Date de dépôt :</strong> 2026-06-06 14:15:00</p>
+            <p><strong>Date limite :</strong> 2026-06-05 23:59:00</p>
+        </div>
+    </div>
+</div>
